458220293bdcf7e0680530e7c2a0cae3c1b4639b
JOCASTAOFF <49947299+jocastaoff@users.noreply.github.com>
2023-12-20 21:19:10 +0100

    pacman.js

--- pacman.js
+++ pacman.js
@@ -1,99 +1,81 @@
-import pygame
-import random
+function init() {
+  // Créer un canvas
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
 
-# Initialisation de Pygame
-pygame.init()
+  // Créer un labyrinthe
+  var labyrinthe = new Labyrinthe(ctx);
 
-# Définition des dimensions de la fenêtre du jeu
-largeur = 800
-hauteur = 600
-fenetre = pygame.display.set_mode((largeur, hauteur))
-pygame.display.set_caption("Pacman Chasse Fantômes")
+  // Créer un pacman
+  var pacman = new Pacman(ctx, labyrinthe);
 
-# Couleurs
-BLANC = (255, 255, 255)
-JAUNE = (255, 255, 0)
-ROUGE = (255, 0, 0)
-BLEU = (0, 0, 255)
+  // Créer des fantômes
+  var fantomes = [];
+  for (var i = 0; i < 5; i++) {
+    var fantome = new Fantome(ctx, labyrinthe);
+    fantomes.push(fantome);
+  }
 
-# Classe pour le Pacman
-class Pacman(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("pacman.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.center = (largeur // 2, hauteur // 2)
-        self.vitesse = 5
+  // Démarrer le jeu
+  startGame();
+}
 
-    def update(self):
-        touches = pygame.key.get_pressed()
-        if touches[pygame.K_LEFT]:
-            self.rect.x -= self.vitesse
-        if touches[pygame.K_RIGHT]:
-            self.rect.x += self.vitesse
-        if touches[pygame.K_UP]:
-            self.rect.y -= self.vitesse
-        if touches[pygame.K_DOWN]:
-            self.rect.y += self.vitesse
+function startGame() {
+  // Démarrer la boucle de jeu
+  var loop = setInterval(update, 1000 / 60);
 
-        # Vérifier les collisions avec les fantômes
-        collisions = pygame.sprite.spritecollide(self, fantomes, False)
-        if collisions:
-            pygame.quit()
+  // Gérer les événements clavier
+  document.addEventListener("keydown", function(event) {
+    switch (event.keyCode) {
+      case 37: // Gauche
+        pacman.moveLeft();
+        break;
+      case 38: // Haut
+        pacman.moveUp();
+        break;
+      case 39: // Droite
+        pacman.moveRight();
+        break;
+      case 40: // Bas
+        pacman.moveDown();
+        break;
+    }
+  });
+}
 
-# Classe pour les Fantômes
-class Fantome(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((30, 30))
-        self.image.fill(ROUGE)
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, largeur - self.rect.width)
-        self.rect.y = random.randint(0, hauteur - self.rect.height)
-        self.vitesse = random.randint(1, 3)
-        self.direction_x = random.choice([-1, 1])
-        self.direction_y = random.choice([-1, 1])
+function update() {
+  // Mettre à jour le pacman
+  pacman.update();
 
-    def update(self):
-        self.rect.x += self.vitesse * self.direction_x
-        self.rect.y += self.vitesse * self.direction_y
+  // Mettre à jour les fantômes
+  for (var i = 0; i < fantomes.length; i++) {
+    fantomes[i].update();
+  }
 
-        # Faire rebondir les fantômes sur les bords de l'écran
-        if self.rect.right >= largeur or self.rect.left <= 0:
-            self.direction_x *= -1
-        if self.rect.bottom >= hauteur or self.rect.top <= 0:
-            self.direction_y *= -1
+  // Vérifier les collisions
+  for (var i = 0; i < fantomes.length; i++) {
+    if (pacman.collidesWith(fantomes[i])) {
+      // Le pacman a été touché par un fantôme
+      gameOver();
+    }
+  }
 
-# Création des groupes de sprites
-pacman = pygame.sprite.GroupSingle(Pacman())
-fantomes = pygame.sprite.Group()
-for _ in range(5):
-    fantome = Fantome()
-    fantomes.add(fantome)
+  // Dessiner le labyrinthe
+  labyrinthe.draw();
 
-# Boucle de jeu
-running = True
-clock = pygame.time.Clock()
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+  // Dessiner le pacman
+  pacman.draw();
 
-    pacman.update()
-    fantomes.update()
+  // Dessiner les fantômes
+  for (var i = 0; i < fantomes.length; i++) {
+    fantomes[i].draw();
+  }
+}
 
-    # Dessiner le fond de la fenêtre
-    fenetre.fill(BLANC)
+function gameOver() {
+  // Arrêter la boucle de jeu
+  clearInterval(loop);
 
-    # Dessiner les sprites
-    pacman.draw(fenetre)
-    fantomes.draw(fenetre)
-
-    # Rafraîchir l'écran
-    pygame.display.flip()
-
-    # Limiter la vitesse de rafraîchissement
-    clock.tick(60)
-
-# Quitter Pygame
-pygame.quit()
+  // Afficher un message de game over
+  alert("Game over !");
+}
